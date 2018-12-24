75d7622997e024d329b5f6c00fc09d36f33412c9
Akiva <calculatinghchance2@live.com>
2018-12-24 09:14:51 -0500

    partially working code

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,4 +1,16 @@
 {
+
   $('#album-title').text(album.title);
   $('img#album-cover-art').attr('src', album.albumArtUrl)
+  $('.artist').text(album.artist);
+  $('#release-info').text(album.releaseInfo);
+  $('#song-list').each(function(album){
+    var index = 1;
+  //  "song-number-column": index;
+    $("#song-number-column").text(index);
+    $("#song-title-column").text(album.songs.title);
+    $("#song-duration-column").text(album.songs.duration);
+    index++;
+  });
+
 }
