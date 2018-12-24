9b27d63f5f4242b9cd18a6dbc4d4cee78e67c433
Akiva <calculatinghchance2@live.com>
2018-12-24 10:33:53 -0500

    Saved and trying to merge from branch to master

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -4,13 +4,15 @@
   $('img#album-cover-art').attr('src', album.albumArtUrl)
   $('.artist').text(album.artist);
   $('#release-info').text(album.releaseInfo);
-  $('#song-list').each(function(album){
-    var index = 1;
-  //  "song-number-column": index;
-    $("#song-number-column").text(index);
-    $("#song-title-column").text(album.songs.title);
-    $("#song-duration-column").text(album.songs.duration);
-    index++;
-  });
+
+  //This is broken code I can work off for the next checkpoint to add things to columns
+  // $('#song-list').each(function(album){
+  //   var index = 1;
+  // //  "song-number-column": index;
+  //   $("#song-number-column").text(index);
+  //   $("#song-title-column").text(album.songs.title);
+  //   $("#song-duration-column").text(album.songs.duration);
+  //   index++;
+  // });
 
 }
