a35f2defa40b851188a4cae880bac76a90e4df43
Akiva <calculatinghchance2@live.com>
2019-01-07 10:53:55 -0500

    fixed bug where loaded song duration of previous song

--- scripts/helper.js
+++ scripts/helper.js
@@ -2,10 +2,10 @@
     class Helper {
     playPauseAndUpdate(song){
     //  if (player.playState !== 'playing') { return; }
-    //  player.playPause(song)
-      const duration = player.getDuration();
+     player.playPause(song)
+      const duration = player.currentlyPlaying.duration;
       $('#time-control .total-time').text( duration );
-        player.playPause(song)
+    //    player.playPause(song)
     }
   };
 
