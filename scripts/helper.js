1df7aa2ea99d1f275c48e7f56da790105fea9b5b
Akiva <calculatinghchance2@live.com>
2019-01-07 10:16:38 -0500

    changed order of player.playPause for bug fix check

--- scripts/helper.js
+++ scripts/helper.js
@@ -2,9 +2,10 @@
     class Helper {
     playPauseAndUpdate(song){
     //  if (player.playState !== 'playing') { return; }
-      player.playPause(song)
+    //  player.playPause(song)
       const duration = player.getDuration();
       $('#time-control .total-time').text( duration );
+        player.playPause(song)
     }
   };
 
