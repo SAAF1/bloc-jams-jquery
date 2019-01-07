2b4e3d4c5afad5110dfcbe51efdf33a1b34f5f21
Akiva <calculatinghchance2@live.com>
2019-01-07 10:00:20 -0500

    All assignments completed correctly and optimally

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,26 +1,12 @@
 {
-  // var helper = class {};
-
-  class helper {
+    class Helper {
     playPauseAndUpdate(song){
-      if (player.playState !== 'playing') { return; }
+    //  if (player.playState !== 'playing') { return; }
+      player.playPause(song)
       const duration = player.getDuration();
       $('#time-control .total-time').text( duration );
-
-      player.playPause(song) =
     }
   };
 
-  var helper = new helper();
-
-//For Reference. Delete Later.
-//  setInterval( () => {
-  //   if (player.playState !== 'playing') { return; }
-  //   const currentTime = player.getTime();
-  //   const duration = player.getDuration();
-  //   const percent = (currentTime / duration) * 100;
-  //   $('#time-control .current-time').text( currentTime );
-  //   $('#time-control input').val(percent);
-  // }, 1000);
-  //end Delete
+  var helper = new Helper();
 }
